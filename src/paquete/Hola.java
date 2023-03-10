7f86ddb0be112ee971ffdfaaf836bafa165545a1
sergioMB92 <125674484+sergioMB92@users.noreply.github.com>
2023-03-10 17:53:31 +0100

    Update Hola.java

--- src/paquete/Hola.java
+++ src/paquete/Hola.java
@@ -9,6 +9,7 @@ public class Hola {
         System.out.println("Hola Mundo");
         //añade aqui otro System.out.println con tu nombre completo
 		System.out.println("Jorge CC");
+	    	System.out.println("SergioMB");
     }
 	
 
